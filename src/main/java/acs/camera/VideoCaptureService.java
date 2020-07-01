f677fcf5261f387eab62d014d776ddd4de74560b
John Doe <xevoxe@gmail.com>
2020-07-01 18:56:11 -0500

    Refactor VideoCaptureService

---
+++ src/main/java/acs/camera/VideoCaptureService.java
@@ -0,0 +1,18 @@
+package acs.camera;
+
+import org.opencv.videoio.VideoCapture;
+
+public class VideoCaptureService implements Runnable {
+
+    private VideoCapture vCapture;
+
+
+    public VideoCaptureService(){
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
