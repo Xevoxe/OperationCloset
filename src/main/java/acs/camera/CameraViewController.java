f712cebdc53d7246a6730cacf3f8b899fd96f56b
John Doe <xevoxe@gmail.com>
2020-07-02 06:18:54 -0500

    Refactor CameraViewController added VideoCaptureService

--- src/main/java/acs/camera/CameraViewController.java
+++ src/main/java/acs/camera/CameraViewController.java
@@ -33,20 +33,24 @@ public class CameraViewController implements Initializable {
     @FXML
     private ImageView videoFrame; //Holds the current frame from capture stream
 
-    private VideoCapture videoCapture; //Captures the video
     private int cameraId;   // Id of the camera to use
-    private ScheduledExecutorService videoService = null; //Thread to update Camera View
+    VideoCaptureService videoCaptureService;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        //Loads in images
+        this.cameraId = CameraSettings.DEFAULT_CAMERA;
+
         setUpScene();
 
-        //Sets up video capture
-        setupVideoCapture();
+        videoCaptureService = new VideoCaptureService(videoFrame);
 
+        try {
+            videoCaptureService.StartService(cameraId);
+        } catch (VideoCaptureService.CameraInUse cameraInUse) {
+            cameraInUse.printStackTrace();
+        }
     }
 
 
@@ -54,7 +58,6 @@ public class CameraViewController implements Initializable {
      *  Loads in any images and sets up scene elements.
      */
     private void setUpScene(){
-
 
         //Load in Icons
         Image switchCamIcon = null;
@@ -72,82 +75,6 @@ public class CameraViewController implements Initializable {
         videoFrame.setPreserveRatio(true);
     }
 
-    private void setupVideoCapture(){
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME); //Load native OpenCV Library
-
-        this.videoCapture = new VideoCapture();
-
-        //TODO Change the 0 to variable of chosen camera
-        this.videoCapture.open(0,Videoio.CAP_DSHOW);
-
-        //Set the maximum resolution possible for camera being used
-        this.videoCapture.set(3,10000);
-        this.videoCapture.set(4,10000);
-
-        //Get aspect ratio of camera
-        System.out.println("Aspect Ratio: " + this.videoCapture.get(3) + " X " + this.videoCapture.get(4));
-
-
-        //TODO add Exception that another program might be using camera.
-        if(this.videoCapture.isOpened()) {
-            System.out.println("Video Stream available");
-
-            /**
-             * Creates a thread that will get a Frame every 30 seconds
-             */
-            Runnable getFrames = () -> {
-                //Prevent race condition in case of camera switch
-                if (this.videoCapture.isOpened()) {
-
-                    //Re-enable button after camera switch
-                    this.changeCameraBtn.setDisable(false);
-                    Mat frame = getFrame();
-
-                    //Crop Frame
-                    int centerX = frame.width() / 2;
-                    int centerY = frame.height() / 2;
-
-//                System.out.println("X: " + centerX + "Y: " + centerY);
-
-                    //TODO: Uncomment following lines only there to handle different aspect rations to show image.
-//                Rect rectCrop = new Rect(centerX - 247 ,0,495,720);
-//                frame = frame.submat(rectCrop);
-
-
-                    BufferedImage image = new BufferedImage(frame.width(), frame.height(), BufferedImage.TYPE_3BYTE_BGR);
-                    final byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-                    frame.get(0, 0, pixels);
-
-                    //Update ImageView on FXThread
-                    Platform.runLater(() -> {
-                        videoFrame.setImage(SwingFXUtils.toFXImage(image, null));
-                    });
-                }
-            };
-
-            this.videoService = Executors.newSingleThreadScheduledExecutor();
-            this.videoService.scheduleAtFixedRate(getFrames, 0, 33, TimeUnit.MILLISECONDS);
-        }
-
-    }
-
-
-    /**
-     * Get a frame from the opened video stream if available
-     * @return the frame to show
-     */
-    private Mat getFrame() {
-
-        Mat frame = new Mat();
-
-        if (this.videoCapture.isOpened()) {
-            //Read the current frame
-            this.videoCapture.read(frame);
-
-        }
-
-        return frame;
-    }
 
     /**
      * Toggles between front and back cameras.
@@ -156,26 +83,17 @@ public class CameraViewController implements Initializable {
     private void toggleCamera(){
         this.cameraId = this.cameraId == 0 ? 1 : 0;
 
-        this.videoCapture.open(this.cameraId,Videoio.CAP_DSHOW); //Open new camera Id
+        this.videoCaptureService.setCamera(this.cameraId);
+        //Open new camera Id
 
         //Disable this button until new camera is loaded.
-        this.changeCameraBtn.setDisable(true);
+//        this.changeCameraBtn.setDisable(true);
     }
 
 
     public void shutdown() {
         System.out.println("Shutdown Video Service and release camera");
-        this.videoCapture.release();
-        this.videoService.shutdown();
-
-        try {
-            if(!this.videoService.awaitTermination((long) 41.66,TimeUnit.MILLISECONDS)){
-                System.out.println("Shutdown Video Service Now");
-                this.videoService.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        this.videoCaptureService.ShutDown();
     }
 
     public void captureImage(ActionEvent actionEvent) {
--- src/main/java/acs/camera/VideoCaptureService.java
+++ src/main/java/acs/camera/VideoCaptureService.java
@@ -1,18 +1,156 @@
 package acs.camera;
 
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.ImageView;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.video.Video;
 import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.Videoio;
 
-public class VideoCaptureService implements Runnable {
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+public class VideoCaptureService {
+
+    private int selectedCamera; //Index of the selected camera
+
 
     private VideoCapture vCapture;
+    private final Consumer<Mat> matConsumer; //Consume Mat from CapturedThread
+    private CaptureThread captureThread;    //Separate Thread to Capture images from camera
+    ScheduledExecutorService threadService;
 
 
-    public VideoCaptureService(){
+    public VideoCaptureService(ImageView imageView){
+
+        this.selectedCamera = 0; //Select Default Camera
+
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME); //Load native OpenCV Library
+
+        this.vCapture = new VideoCapture();
+
+        //Create Consumer
+        matConsumer = mat -> {
+
+            BufferedImage image = createImageFromMat(mat);
+            //Set JavaFX Element Image to Frame
+            imageView.setImage(SwingFXUtils.toFXImage(image, null));
+        };
 
     }
 
-    @Override
-    public void run() {
+    public void StartService(int cameraIndex) throws CameraInUse {
+        if(this.vCapture.open(cameraIndex, Videoio.CAP_DSHOW)){
+            this.selectedCamera = cameraIndex;
+            captureThread = new CaptureThread(matConsumer,vCapture);
 
+            //Start the Thread
+            //Executes captureThread
+            threadService = Executors.newSingleThreadScheduledExecutor();
+            threadService.scheduleAtFixedRate(captureThread,0,33, TimeUnit.MILLISECONDS);
+        }else{
+            throw new CameraInUse();
+        }
+    }
+
+    /**
+     * Shut down Video Capture thread and release camera
+     */
+    public void ShutDown(){
+        this.vCapture.release();
+        this.threadService.shutdown();
+
+        try {
+            if(!this.threadService.awaitTermination((long) 41.66,TimeUnit.MILLISECONDS)){
+                System.out.println("Shutdown Video Service Now");
+                this.threadService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setCamera(int index){
+        if(index != selectedCamera){
+            this.selectedCamera = index;
+            this.vCapture.open(selectedCamera,Videoio.CAP_DSHOW);
+        }
+    }
+
+    /**
+     * Pause Video Capture
+     * @throws InterruptedException
+     */
+    private synchronized void PauseVideoCapture() throws InterruptedException {
+        captureThread.wait();
+    }
+
+    /**
+     * Restart video capture
+     */
+    private synchronized void ResumeVideoCapture(){
+        captureThread.notify();
+    }
+
+    private BufferedImage createImageFromMat(Mat frame){
+        //Crop Frame
+        int centerX = frame.width() / 2;
+        int centerY = frame.height() / 2;
+
+//                System.out.println("X: " + centerX + "Y: " + centerY);
+
+        //TODO: Uncomment following lines only there to handle different aspect rations to show image.
+//                Rect rectCrop = new Rect(centerX - 247 ,0,495,720);
+//                frame = frame.submat(rectCrop);
+
+        BufferedImage image = new BufferedImage(frame.width(), frame.height(), BufferedImage.TYPE_3BYTE_BGR);
+        final byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        frame.get(0, 0, pixels);
+
+        return image;
+    }
+
+    /**
+     * Separate thread to capture images from selected camera
+     */
+    private static class CaptureThread implements Runnable {
+
+        Consumer<Mat> matConsumer; // Handle new frame.
+        VideoCapture vCapture;
+
+        /**
+         *
+         * @param sourceConsumer Callback consumer for JavaFXThread
+         * @param vCapture VideoCapture from OpenCV
+         */
+        public CaptureThread(Consumer<Mat> sourceConsumer,VideoCapture vCapture) {
+            this.vCapture = vCapture; //Instantiate a new video capture object
+            this.matConsumer = sourceConsumer;
+        }
+        @Override
+        public void run() {
+            if (this.vCapture.isOpened()) {
+                Mat frame = new Mat();
+                this.vCapture.read(frame); //Get frame from camera
+
+                //Handle new Mat on JavaFX Thread
+                Platform.runLater(() -> {
+                    matConsumer.accept(frame); //JavaFX Thread consumes mat.
+                });
+            }
+        }
+    }
+
+    public static class CameraInUse extends Exception{
+        public CameraInUse(){
+            super("Camera in use by another application");
+        }
     }
 }
